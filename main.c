b8bf4cf3f420a26d0842f716b132251049585710
Vinícius Nunes <111578363+ViniciYou@users.noreply.github.com>
2024-02-26 10:50:47 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,21 +1,91 @@
 #include <stdio.h>
 
 int main() {
-    //Imprime toda tabela de caracteres
-    /*for (int i=0; i < 256; i++) {
-        printf("Inteiro %d ======> Caractere %c \n",i ,i);
-    }*/
 
-    char letra1, letra2;
-    printf("Digite a letra 1:\n");
-    //Limpar o buffer
+
+    //Verifica se o valor é par ou impar
+    /*int x;
+    printf("Digite um valor inteiro: \n");
+    scanf("%d", &x);
+
+    //operador ternario
+
+    //condição ? valor se verdadeiro : valor se falso;
+    x % 2 == 0 ? printf("par\n") : printf("impar\n");
+    */
+
+    /*-----------------------------------------------------------------*/
+
+    /*char produtividade;
+    double salarioAtual, salarioReajustado;
+
+    //Programa:
+    //se o ususario cumpriu a produtividade
+    //aumenta-se 10% se não 5% no seu salario.
+
     fflush(stdin);
-    letra1 = getchar();
-    printf("Digite a letra 2:\n");
+    printf("Digite 's' ou 'S' para produtividade, se atingiu.\n");
+    produtividade = getchar();
     fflush(stdin);
-    letra2 = getchar();
+    printf("Digite o salario atual:\n");
+    scanf("%lf",&salarioAtual);
+    salarioReajustado = produtividade == 's' || produtividade == 'S' ? salarioAtual * 1.1 : salarioAtual * 1.05;
 
-    printf("Letra 1: %c e Letra 2: %c \n", letra1, letra2);
+    printf("Novo salario: %.2lf \n", salarioReajustado);
+    */
 
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    //Dias da semana usando switch
+    /*int valor;
+
+    printf("Digite um valor de um a sete: \n");
+    scanf("%d", &valor);
+    switch (valor) {
+        case 1:
+            printf("Domingo!");
+            break;
+        case 2:
+            printf("Segunda-Feira!");
+            break;
+        case 3:
+            printf("Terca-Feira!");
+            break;
+        case 4:
+                printf("Quarta-Feira!");
+            break;
+        case 5:
+            printf("Quinta-Feira!");
+            break;
+        case 6:
+            printf("Sexta-Feira!");
+            break;
+        case 7:
+            printf("Sabado!");
+            break;
+        default:
+            printf("Opcao Invalida!");
+    }
+     */
+
+    /*---------------------------------------------------------------------------------------------------*/
+
+
+    //descobrir se letra e vogal usando switch
+    /*char letra;
+    printf("Digite uma letra minuscula: \n");
+    letra = getchar();
+    switch (letra) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            printf("E vogal \n");
+            break;
+        default:
+            printf("Nao e vogal!\n");
+    }
+    */
     return 0;
 }
