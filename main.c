ff014bfe37b20c220f86cef93864ff0bc50b48e5
Vinícius Nunes <111578363+ViniciYou@users.noreply.github.com>
2024-02-26 09:06:08 -0300

    Cálculo de área e Circunferencia
    Cálculo de área e Circunferencia de circulo digitado por usuario

--- main.c
+++ main.c
@@ -1,31 +1,16 @@
 #include <stdio.h>
+#define PI 3.1415
 
 int main() {
+    double perimetro, area, raio;
+    printf("Digite o raio da circuferencia \n");
+    scanf("%lf", &raio);
 
-    int x = 10;
-    int y = 15;
+    perimetro = PI * raio * raio;
+    area = 2 * PI * raio;
 
-    //valor
-    printf("Valor de x: %d \n", x);
-    printf("Valor de x: %d \n", y);
-
-    //endereço
-    printf("Endereco de x: %d \n", &y);
-    printf("Endereco de y: %d \n", &y);
-
-
-    //A contagem dos números varia se o sinal de mais ou menos está a esquerda ou a direita da variavel
-    int z = ++x + ++y;
-    printf("Valor de z: %d \n", z);
-    printf("Valor de x: %d \n", x);
-    printf("Valor de x: %d \n", y);
-
-    //Ver tamanhos
-    /*int a = 10;
-    double x = 13.34;
-    printf("Tamanho em bytes do int: %ld \n", sizeof(int));
-    printf("Tamanho em bytes do double: %ld \n", sizeof(double));
-    */
+    printf("Perimetro calculado: %.2lf \n", perimetro);
+    printf("Area calculada: %.2lf \n", area);
 
     return 0;
 }
